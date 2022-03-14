cecee5dc32190f8774f857df5c5ec01e5883a7b9
emiz98 <64089619+emiz98@users.noreply.github.com>
2022-03-14 19:57:15 +0530

    Timeout Fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -31,7 +31,7 @@ export default function Home() {
   }, [session]);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 0);
+    setTimeout(() => setLoading(false), 5000);
   }, []);
 
   if (status === "loading") {
